73500e955fcd788193d4d6c2d97671f4f9220b15
jiehanzheng <jiehanzheng@2066f16a-a1ef-0310-b9f2-d44eba5528fd>
2010-08-13 05:10:34 +0000

    WordPress 3.0.1 Translations

--- dist/wp-admin/setup-config.php
+++ dist/wp-admin/setup-config.php
@@ -17,10 +17,14 @@
 define('WP_INSTALLING', true);
 
 /**
+ * We are blissfully unaware of anything.
+ */
+define('WP_SETUP_CONFIG', true);
+
+/**
  * Disable error reporting
  *
- * Set this to error_reporting( E_ALL ) or error_reporting( E_ALL | E_STRICT ) f
-or debugging
+ * Set this to error_reporting( E_ALL ) or error_reporting( E_ALL | E_STRICT ) for debugging
  */
 error_reporting(0);
 
@@ -32,30 +36,33 @@ error_reporting(0);
 define('ABSPATH', dirname(dirname(__FILE__)).'/');
 define('WPINC', 'wp-includes');
 define('WP_CONTENT_DIR', ABSPATH . 'wp-content');
+define('WP_DEBUG', false);
 /**#@-*/
 
+require_once(ABSPATH . WPINC . '/load.php');
 require_once(ABSPATH . WPINC . '/compat.php');
 require_once(ABSPATH . WPINC . '/functions.php');
 require_once(ABSPATH . WPINC . '/classes.php');
+require_once(ABSPATH . WPINC . '/version.php');
 
 if (!file_exists(ABSPATH . 'wp-config-sample.php'))
-	wp_die('提示！未能检测到 wp-config-sample.php 文件，请确认根目录存在此文件或重新上传。');
+	wp_die('抱歉，我需要 wp-config-sample.php 文件作为向导工作的基础，请重新上传此文件。');
 
 $configFile = file(ABSPATH . 'wp-config-sample.php');
 
 // Check if wp-config.php has been created
 if (file_exists(ABSPATH . 'wp-config.php'))
-	wp_die("<p>'wp-config.php'文件已存在。如果您想更改 wp-config.php 内已有的设定，请先删除它，本向导会重新创建 wp-config.php 。<a href='install.php'>重试</a>。</p>");
+	wp_die("<p>'wp-config.php' 文件已存在。若您希望重新配置这个安装，请先删除它。您可以尝试<a href='install.php'>现在安装</a>。</p>");
 
 // Check if wp-config.php exists above the root directory but is not part of another install
 if (file_exists(ABSPATH . '../wp-config.php') && ! file_exists(ABSPATH . '../wp-settings.php'))
-	wp_die("<p>'wp-config.php'已存在于更高一级的目录内。如果您想更改 wp-config.php 内已有的设定，请先删除它，本向导会重新创建wp-config.php。<a href='install.php'>重试</a>。</p>");
+	wp_die("<p>'wp-config.php' 文件错误地存在于上级目录。若您希望重新配置这个安装，请先删除它。您可以尝试<a href='install.php'>现在安装</a>。</p>");
 
-if ( version_compare( '4.3', phpversion(), '>' ) )
-	wp_die( sprintf( /*WP_I18N_OLD_PHP*/'您服务器的PHP版本为 %s 但 WordPress 至少需要 4.3.'/*/WP_I18N_OLD_PHP*/, phpversion() ) );
+if ( version_compare( $required_php_version, phpversion(), '>' ) )
+	wp_die( sprintf( /*WP_I18N_OLD_PHP*/'您正使用 PHP %1$s，但 WordPress 需要其版本高于 %2$s 才可工作。'/*/WP_I18N_OLD_PHP*/, phpversion(), $required_php_version ) );
 
 if ( !extension_loaded('mysql') && !file_exists(ABSPATH . 'wp-content/db.php') )
-	wp_die( /*WP_I18N_OLD_MYSQL*/'您的PHP似乎没有 MySQL 拓展。'/*/WP_I18N_OLD_MYSQL*/ );
+	wp_die( /*WP_I18N_OLD_MYSQL*/'您的 PHP 似乎确实 WordPress 所需的 MySQL 插件。'/*/WP_I18N_OLD_MYSQL*/ );
 
 if (isset($_GET['step']))
 	$step = $_GET['step'];
@@ -77,7 +84,7 @@ function display_header() {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>WordPress &rsaquo; Setup Configuration File</title>
+<title>WordPress &rsaquo; 设置配置文件</title>
 <link rel="stylesheet" href="css/install.css" type="text/css" />
 
 </head>
@@ -91,19 +98,18 @@ switch($step) {
 		display_header();
 ?>
 
-<p>欢迎来到 WordPress 的世界！正式开始之前，我们需要一些关于数据库的信息。请确认您已经拥有以下信息：</p>
+<p>欢迎使用 WordPress！在您开始使用前，WordPress 需要一些数据库的信息。下列信息将会被问到，请作好准备。</p>
 <ol>
-	<li>数据库名称</li>
-	<li>数据库用户名</li>
-	<li>数据库密码</li>
-	<li>数据库主机地址</li>
-	<li>数据表前缀（如果您需要在同一数据库内安装多个 WordPress 的话）</li>
+	<li>数据库名</li>
+	<li>数据库用户用户名</li>
+	<li>数据库用户密码</li>
+	<li>数据库主机</li>
+	<li>表名前缀（若您希望在一个数据表中安装多个 WordPress）</li>
 </ol>
-<p><strong>如果无法进入下一步，别着急。此向导的目的在于创建 Wordpress 的配置文件，所以您还可以直接用文本编辑器打开 <code>wp-config-sample.php</code>，根据提示填写相应信息，然后保存并将它重命名为 <code>wp-config.php</code>。</strong></p>
-<p>正常情况下，您的空间商会告知数据库的有关信息。如果您不太清楚，请先联系您的空间提供商。如果已经准备好了 &hellip;</p>
+<p><strong>出于各种原因，自动创建文件可能失败，但本向导只是助您在配置文件中写如数据库信息。此时，您只需用文本编辑器打开 <code>wp-config-sample.php</code>，填入您的信息，另存为 <code>wp-config.php</code> 即可。</strong></p>
+<p>大多数的互联网主机服务提供商都向您提供了数据库的信息。若您不知道这些信息，您需要先询问好，再进行安装。若您已准备好 &hellip;</p>
 
-
-<p class="step"><a href="setup-config.php?step=1" class="button">开始!</a></p>
+<p class="step"><a href="setup-config.php?step=1<?php if ( isset( $_GET['noapi'] ) ) echo '&amp;noapi'; ?>" class="button">现在就开始！</a></p>
 <?php
 	break;
 
@@ -111,34 +117,35 @@ switch($step) {
 		display_header();
 	?>
 <form method="post" action="setup-config.php?step=2">
-	<p>您应该在下面的表单中填入对应的数据库信息，不确定的项目请联系您的空间提供商确认。 </p>
+	<p>请在下方输入数据库相关信息。若您不清楚，请咨询主机提供商。</p>
 	<table class="form-table">
 		<tr>
-			<th scope="row"><label for="dbname">数据库名称</label></th>
+			<th scope="row"><label for="dbname">数据库名</label></th>
 			<td><input name="dbname" id="dbname" type="text" size="25" value="wordpress" /></td>
-			<td>用于存储 WordPress 数据的数据库名称。</td>
+			<td>您希望 WordPress 使用哪个数据库运行？</td>
 		</tr>
 		<tr>
-			<th scope="row"><label for="uname">数据库用户名</label></th>
+			<th scope="row"><label for="uname">用户名</label></th>
 			<td><input name="uname" id="uname" type="text" size="25" value="username" /></td>
 			<td>您的 MySQL 用户名</td>
 		</tr>
 		<tr>
-			<th scope="row"><label for="pwd">数据库密码</label></th>
+			<th scope="row"><label for="pwd">密码</label></th>
 			<td><input name="pwd" id="pwd" type="text" size="25" value="password" /></td>
-			<td>...以及 MySQL 密码</td>
+			<td>... 以及 MySQL 密码。</td>
 		</tr>
 		<tr>
-			<th scope="row"><label for="dbhost">数据库地址</label></th>
+			<th scope="row"><label for="dbhost">数据库主机</label></th>
 			<td><input name="dbhost" id="dbhost" type="text" size="25" value="localhost" /></td>
-			<td>大部分主机无需修改。</td>
+			<td>通常情况下，应填写 <code>localhost</code>，若测试连接失败，请联系主机提供商咨询。</td>
 		</tr>
 		<tr>
-			<th scope="row"><label for="prefix">数据表前缀</label></th>
+			<th scope="row"><label for="prefix">表名前缀</label></th>
 			<td><input name="prefix" id="prefix" type="text" id="prefix" value="wp_" size="25" /></td>
-			<td>如果有在同一数据库内安装多个 WordPress 的需求请更改此项。</td>
+			<td>若您希望在一个数据库中存放多个 WordPress 的数据，请修改本项以做区分。</td>
 		</tr>
 	</table>
+	<?php if ( isset( $_GET['noapi'] ) ) { ?><input name="noapi" type="hidden" value="true" /><?php } ?>
 	<p class="step"><input name="submit" type="submit" value="提交" class="button" /></p>
 </form>
 <?php
@@ -150,7 +157,12 @@ switch($step) {
 	$passwrd = trim($_POST['pwd']);
 	$dbhost  = trim($_POST['dbhost']);
 	$prefix  = trim($_POST['prefix']);
-	if (empty($prefix)) $prefix = 'wp_';
+	if ( empty($prefix) )
+		$prefix = 'wp_';
+
+	// Validate $prefix: it can only contain letters, numbers and underscores
+	if ( preg_match( '|[^a-z0-9_]|i', $prefix ) )
+		wp_die( /*WP_I18N_BAD_PREFIX*/'<strong>错误</strong>：“表名前缀”只能含有数字、字母以及下划线。'/*/WP_I18N_BAD_PREFIX*/ );
 
 	// Test the db connection.
 	/**#@+
@@ -167,16 +179,49 @@ switch($step) {
 	if ( !empty($wpdb->error) )
 		wp_die($wpdb->error->get_error_message());
 
+	// Fetch or generate keys and salts.
+	$no_api = isset( $_POST['noapi'] );
+	require_once( ABSPATH . WPINC . '/plugin.php' );
+	require_once( ABSPATH . WPINC . '/l10n.php' );
+	require_once( ABSPATH . WPINC . '/pomo/translations.php' );
+	if ( ! $no_api ) {
+		require_once( ABSPATH . WPINC . '/class-http.php' );
+		require_once( ABSPATH . WPINC . '/http.php' );
+		wp_fix_server_vars();
+		/**#@+
+		 * @ignore
+		 */
+		function get_bloginfo() {
+			return ( ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'] . str_replace( $_SERVER['PHP_SELF'], '/wp-admin/setup-config.php', '' ) );
+		}
+		/**#@-*/
+		$secret_keys = wp_remote_get( 'https://api.wordpress.org/secret-key/1.1/salt/' );
+	}
+
+	if ( $no_api || is_wp_error( $secret_keys ) ) {
+		$secret_keys = array();
+		require_once( ABSPATH . WPINC . '/pluggable.php' );
+		for ( $i = 0; $i < 8; $i++ ) {
+			$secret_keys[] = wp_generate_password( 64, true, true );
+		}
+	} else {
+		$secret_keys = explode( "\n", wp_remote_retrieve_body( $secret_keys ) );
+		foreach ( $secret_keys as $k => $v ) {
+			$secret_keys[$k] = substr( $v, 28, 64 );
+		}
+	}
+	$key = 0;
+
 	foreach ($configFile as $line_num => $line) {
 		switch (substr($line,0,16)) {
 			case "define('DB_NAME'":
-				$configFile[$line_num] = str_replace("putyourdbnamehere", $dbname, $line);
+				$configFile[$line_num] = str_replace("database_name_here", $dbname, $line);
 				break;
 			case "define('DB_USER'":
-				$configFile[$line_num] = str_replace("'usernamehere'", "'$uname'", $line);
+				$configFile[$line_num] = str_replace("'username_here'", "'$uname'", $line);
 				break;
 			case "define('DB_PASSW":
-				$configFile[$line_num] = str_replace("'yourpasswordhere'", "'$passwrd'", $line);
+				$configFile[$line_num] = str_replace("'password_here'", "'$passwrd'", $line);
 				break;
 			case "define('DB_HOST'":
 				$configFile[$line_num] = str_replace("localhost", $dbhost, $line);
@@ -184,20 +229,30 @@ switch($step) {
 			case '$table_prefix  =':
 				$configFile[$line_num] = str_replace('wp_', $prefix, $line);
 				break;
+			case "define('AUTH_KEY":
+			case "define('SECURE_A":
+			case "define('LOGGED_I":
+			case "define('NONCE_KE":
+			case "define('AUTH_SAL":
+			case "define('SECURE_A":
+			case "define('LOGGED_I":
+			case "define('NONCE_SA":
+				$configFile[$line_num] = str_replace('put your unique phrase here', $secret_keys[$key++], $line );
+				break;
 		}
 	}
 	if ( ! is_writable(ABSPATH) ) :
 		display_header();
 ?>
-<p>抱歉，无法写入<code>wp-config.php</code>文件。</p>
-<p>您可以手动创建<code>wp-config.php</code>文件，然后复制粘贴下面的文本过去。</p>
-<textarea cols="90" rows="15"><?php
+<p>抱歉，无法写入 <code>wp-config.php</code> 文件。</p>
+<p>您可手动创建 <code>wp-config.php</code>，并复制如下代码至其中。</p>
+<textarea cols="98" rows="15" class="code"><?php
 		foreach( $configFile as $line ) {
-			echo htmlentities($line);
+			echo htmlentities($line, ENT_COMPAT, 'UTF-8');
 		}
 ?></textarea>
-<p>完成后，点击“开始安装”</p>
-<p class="step"><a href="install.php" class="button">开始安装</a></p>
+<p>完成之后，请点击“进行安装”。</p>
+<p class="step"><a href="install.php" class="button">进行安装</a></p>
 <?php
 	else :
 		$handle = fopen(ABSPATH . 'wp-config.php', 'w');
@@ -208,9 +263,9 @@ switch($step) {
 		chmod(ABSPATH . 'wp-config.php', 0666);
 		display_header();
 ?>
-<p>恭喜！WordPress和数据库的连接已经建立。准备好了？ 开始 &hellip;</p>
+<p>非常好！您已顺利完成安装中的这一步骤，WordPress 已经可以和您的数据库沟通。若您准备好了，现在就 &hellip;</p>
 
-<p class="step"><a href="install.php" class="button">开始安装</a></p>
+<p class="step"><a href="install.php" class="button">进行安装</a></p>
 <?php
 	endif;
 	break;
--- dist/wp-config-sample.php
+++ dist/wp-config-sample.php
@@ -1,30 +1,30 @@
 <?php
 /** 
- * WordPress基础配置文件。
+ * WordPress 基础配置文件。
  *
- * 本文件包含以下配置选项: MySQL设置, 数据库表名前缀,
- * 密匙, WordPress语言设定以及ABSPATH. 如需更多信息，请访问
+ * 本文件包含以下配置选项: MySQL 设置、数据库表名前缀、
+ * 密匙、WordPress 语言设定以及 ABSPATH。如需更多信息，请访问
  * {@link http://codex.wordpress.org/Editing_wp-config.php 编辑
- * wp-config.php}Codex. MySQL设置具体信息请咨询您的空间提供商。
+ * wp-config.php} Codex 页面。MySQL 设置具体信息请咨询您的空间提供商。
  *
  * 这个文件用在于安装程序自动生成 wp-config.php 配置文件，
- * 你可以手动复制这个文件，并重命名为 wp-config.php，然后输入相关信息。
+ * 您可以手动复制这个文件，并重命名为 wp-config.php，然后输入相关信息。
  *
  *
  * @package WordPress
  */
 
-// ** MySQL设置 - 具体信息来自您正在使用的主机 ** //
-/** WordPress数据库的名称，替换掉 “putyourdbnamehere” */
-define('DB_NAME', 'putyourdbnamehere');
+// ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
+/** WordPress 数据库的名称 */
+define('DB_NAME', 'database_name_here');
 
-/** MySQL数据库用户名，替换掉 “usernamehere” */
-define('DB_USER', 'usernamehere');
+/** MySQL 数据库用户名 */
+define('DB_USER', 'username_here');
 
-/** MySQL数据库密码，替换掉 “yourpasswordhere” */
-define('DB_PASSWORD', 'yourpasswordhere');
+/** MySQL 数据库密码 */
+define('DB_PASSWORD', 'password_here');
 
-/** MySQL主机名 */
+/** MySQL 主机 */
 define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
@@ -37,18 +37,24 @@ define('DB_COLLATE', '');
  * 身份密匙设定。
  *
  * 您可以随意写一些字符
- * 或者直接访问 {@link https://api.wordpress.org/secret-key/1.1/ WordPress.org Secret-key页面将自动为您生成，任何修改都会导致cookies失效，所以用户必须重新登录}
+ * 或者直接访问 {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org 私钥生成服务}，
+ * 任何修改都会导致 cookie 失效，所有用户必须重新登录。
  *
  * @since 2.6.0
  */
-define('AUTH_KEY', '替换该字符串');
-define('SECURE_AUTH_KEY', '替换该字符串');
-define('LOGGED_IN_KEY', '替换该字符串');
-define('NONCE_KEY', '替换该字符串');
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
 /**#@-*/
 
 /**
- * WordPress数据表前缀。
+ * WordPress 数据表前缀。
  *
  * 如果您有在同一数据库内安装多个 WordPress 的需求，请为每个 WordPress 设置不同的数据表前缀。
  * 前缀名只能为数字、字母加下划线。
@@ -56,19 +62,27 @@ define('NONCE_KEY', '替换该字符串');
 $table_prefix  = 'wp_';
 
 /**
- * WordPress语言设置。默认为英语。
+ * WordPress 语言设置，默认为英语。
  *
- * 本项设定能够让 WordPress 显示您需要的语言。wp-content/languages 内应放置同名的 .mo 语言文件。
+ * 本项设定能够让 WordPress 显示您需要的语言。
+ * 	wp-content/languages 内应放置同名的 .mo 语言文件。
  * 要使用 WordPress 简体中文界面，只需填入 zh_CN。
  */
 define ('WPLANG', 'zh_CN');
 
-/* 设定完毕，请保存该文件。 */
+/**
+ * 开发者专用：WordPress 调试模式。
+ *
+ * 将这个值改为“true”，WordPress 将显示所有开发过程中的提示。
+ * 强烈建议插件开发者在开发环境中启用本功能。
+ */
+define('WP_DEBUG', false);
 
-/** WordPress目录的绝对路径。 */
+/* 好了！请不要再继续编辑。请保存该文件。 */
+
+/** WordPress 目录的绝对路径。 */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** 设置WordPress变量和包含文件。 */
+/** 设置 WordPress 变量和包含文件。 */
 require_once(ABSPATH . 'wp-settings.php');
-?>
